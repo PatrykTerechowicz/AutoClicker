bff3acee50a0798047bd72038d53044cbe5b6c3f
terechsan <terechsama@gmail.com>
2017-08-28 15:03:18 +0200

    Added first window project (no behaviour yet)

--- AutoClicker/src/com/gmail/terechsama/autoclicker/AutoClickerApp.java
+++ AutoClicker/src/com/gmail/terechsama/autoclicker/AutoClickerApp.java
@@ -1,17 +1,30 @@
 package com.gmail.terechsama.autoclicker;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AutoClickerApp extends Application {
+	private static final String LAYOUT_LOCATION = "com/gmail/terechsama/autoclicker/view/rootLayout.fxml";
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void start(Stage primaryStage) throws Exception {
+		initializeLayout(primaryStage);
 	}
 
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	private void initializeLayout(Stage primaryStage) throws IOException {
+		FXMLLoader loader = new FXMLLoader(this.getClass().getResource(LAYOUT_LOCATION));
+		Parent parent = loader.load();
+		Scene scene = new Scene(parent);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 }
--- AutoClicker/src/com/gmail/terechsama/autoclicker/view/RootLayoutController.java
+++ AutoClicker/src/com/gmail/terechsama/autoclicker/view/RootLayoutController.java
@@ -1,7 +1,33 @@
 package com.gmail.terechsama.autoclicker.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
 public class RootLayoutController {
-	
+
+    @FXML
+    private CheckBox atMouseCheckBox;
+
+    @FXML
+    private TextField yCoordinateField;
+
+    @FXML
+    private TextField xCoordinateField;
+
+    @FXML
+    private TextField intervalField;
+
+    @FXML
+    private RadioButton leftRadioButton;
+
+    @FXML
+    private ToggleGroup mouseButton;
+
+    @FXML
+    private RadioButton rightRadioButton;
 	
 	
 	public void initialize() {
